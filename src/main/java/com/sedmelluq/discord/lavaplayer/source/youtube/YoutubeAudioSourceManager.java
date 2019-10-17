dcac1c1662543ac15913a82a3b7c27409e1f627a
Yukine <blazylp@gmail.com>
2019-10-17 23:38:20 +0200

    Bump version

--- src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/YoutubeAudioSourceManager.java
+++ src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/YoutubeAudioSourceManager.java
@@ -130,7 +130,7 @@ public class YoutubeAudioSourceManager implements AudioSourceManager, HttpConfig
       if(request.getURI().toString().contains("generate_204"))
         return;
       request.setHeader("x-youtube-client-name", "1");
-      request.setHeader("x-youtube-client-version", "2.20191008.04.01");
+      request.setHeader("x-youtube-client-version", "2.20191017.02.00");
     };
 
     if (cookieStore == null) {
